d7fe36a3c3b3808aaff71ef62da55790769adc2e
Andrey Ten <444ten@gmail.com>
2015-07-19 18:57:18 +0300

    TSTFriendView -> initial commit

---
+++ 444tenFB/TST/UI/TSTFriendView/TSTFriendView.h
@@ -0,0 +1,13 @@
+//
+//  TSTFriendView.h
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/19/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSTFriendView : UIView
+
+@end
---
+++ 444tenFB/TST/UI/TSTFriendView/TSTFriendView.m
@@ -0,0 +1,21 @@
+//
+//  TSTFriendView.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/19/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TSTFriendView.h"
+
+@implementation TSTFriendView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
