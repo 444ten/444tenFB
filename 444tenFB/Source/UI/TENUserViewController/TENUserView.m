a83cbee5b89d395fa906e747da9a301256618c33
Andrey Ten <444ten@gmail.com>
2015-07-22 16:54:18 +0300

    TENUserView -> initial commit

---
+++ 444tenFB/Source/UI/TENUserViewController/TENUserView.m
@@ -0,0 +1,13 @@
+//
+//  TENUserView.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/22/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENUserView.h"
+
+@implementation TENUserView
+
+@end
