5c796518c96dd3cb7471a33ea69b6a03890d56c1
Andrey Ten <444ten@gmail.com>
2015-07-22 23:09:24 +0300

    TENModel -> initial commit

---
+++ 444tenFB/Source/Models/TENModel/TENModel.h
@@ -0,0 +1,13 @@
+//
+//  TENModel.h
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/22/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENObservableObject.h"
+
+@interface TENModel : TENObservableObject
+
+@end
