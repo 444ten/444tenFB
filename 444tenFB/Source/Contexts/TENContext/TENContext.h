a6ea18475857bd750325dd7170f9bfaf61fbf56b
Andrey Ten <444ten@gmail.com>
2015-07-22 16:34:59 +0300

    TENContext -> initial commit

---
+++ 444tenFB/Source/Contexts/TENContext/TENContext.h
@@ -0,0 +1,13 @@
+//
+//  TENContext.h
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/22/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENObservableObject.h"
+
+@interface TENContext : TENObservableObject
+
+@end
---
+++ 444tenFB/Source/Contexts/TENContext/TENContext.m
@@ -0,0 +1,13 @@
+//
+//  TENContext.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/22/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENContext.h"
+
+@implementation TENContext
+
+@end
