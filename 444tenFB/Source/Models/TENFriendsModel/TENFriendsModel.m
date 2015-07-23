1cbc08c64e281903289e08be96835ac73920c9c7
Andrey Ten <444ten@gmail.com>
2015-07-23 15:35:18 +0300

    TENFriendsModel -> initial commit

---
+++ 444tenFB/Source/Models/TENFriendsModel/TENFriendsModel.m
@@ -0,0 +1,13 @@
+//
+//  TENFriendsModel.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/23/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENFriendsModel.h"
+
+@implementation TENFriendsModel
+
+@end
