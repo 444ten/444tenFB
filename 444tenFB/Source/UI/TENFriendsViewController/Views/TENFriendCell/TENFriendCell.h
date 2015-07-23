57a40910f61156c869593b55fff60616cd75efb1
Andrey Ten <444ten@gmail.com>
2015-07-23 16:55:57 +0300

    TENFriendCell -> initial commit

---
+++ 444tenFB/Source/UI/TENFriendsViewController/Views/TENFriendCell/TENFriendCell.h
@@ -0,0 +1,13 @@
+//
+//  TENFriendCell.h
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/23/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENTableViewCell.h"
+
+@interface TENFriendCell : TENTableViewCell
+
+@end
---
+++ 444tenFB/Source/UI/TENFriendsViewController/Views/TENFriendCell/TENFriendCell.m
@@ -0,0 +1,21 @@
+//
+//  TENFriendCell.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/23/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENFriendCell.h"
+
+@implementation TENFriendCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
