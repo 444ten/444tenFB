bc596032b26b040edcd7b1819e4acc7cefcd86f6
Andrey Ten <444ten@gmail.com>
2015-07-23 15:32:55 +0300

    TENFriendsView -> initial commit

---
+++ 444tenFB/Source/UI/TENFriendsViewController/TENFriendsView.h
@@ -0,0 +1,14 @@
+//
+//  TENFriendsView.h
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/23/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TENFriendsView : UIView
+@property (nonatomic, strong)   IBOutlet UITableView    *friendsTableView;
+
+@end
---
+++ 444tenFB/Source/UI/TENFriendsViewController/TENFriendsView.m
@@ -0,0 +1,21 @@
+//
+//  TENFriendsView.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/23/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENFriendsView.h"
+
+@implementation TENFriendsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
