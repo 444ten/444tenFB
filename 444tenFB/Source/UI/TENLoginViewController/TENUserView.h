f75393f81f68abac4c4fd0ec1786837507d02112
Andrey Ten <444ten@gmail.com>
2015-07-30 14:11:04 +0300

    TENUserView -> initial commit

---
+++ 444tenFB/Source/UI/TENLoginViewController/TENUserView.h
@@ -0,0 +1,13 @@
+//
+//  TENUserView.h
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/30/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TENUserView : UIView
+
+@end
---
+++ 444tenFB/Source/UI/TENLoginViewController/TENUserView.m
@@ -0,0 +1,21 @@
+//
+//  TENUserView.m
+//  444tenFB
+//
+//  Created by Andrey Ten on 7/30/15.
+//  Copyright (c) 2015 444ten. All rights reserved.
+//
+
+#import "TENUserView.h"
+
+@implementation TENUserView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
